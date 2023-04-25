b6dd9b554d412881e3603ddfe66f8fee0dc2a69b
Xuefeng Ding <dingxf@ihep.ac.cn>
2023-04-25 11:16:14 +0800

    unit-test bug fixed. clean up the resource.

--- test/test_service.py
+++ test/test_service.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 import unittest
 from flask_testing import TestCase
-from service import app
+from service import app, ssh_connections, task_queue, workers
 
 class TestApp(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
+
+    @classmethod
+    def tearDownClass(cls):
+        # Stop worker threads
+        for _ in range(len(workers)):
+            task_queue.put(None)
+        for worker in workers:
+            worker.join()
+        # Close SSH connections
+        while not ssh_connections.empty():
+            ssh_client = ssh_connections.get()
+            ssh_client.close()
 
     def test_submit(self):
         data = {"command": "echo 'Hello, World!'"}
